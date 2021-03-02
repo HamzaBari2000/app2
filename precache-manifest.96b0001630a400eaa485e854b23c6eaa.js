781fae49c45c8829fb92bbee7fbf755897809679
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-03-02 04:10:24 +0000

    deploy

---
+++ precache-manifest.96b0001630a400eaa485e854b23c6eaa.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be1563db820c4eb72eb8215879cb810",
+    "url": "/app2/IT.png"
+  },
+  {
+    "revision": "3060f1edfc2fdc9c76661ce1e65eceda",
+    "url": "/app2/Logo.png"
+  },
+  {
+    "revision": "ede8216bcf7a517c18d0ce60769fcd27",
+    "url": "/app2/PhyicalEducation.png"
+  },
+  {
+    "revision": "7de1524dc6e04749931920474056b7c0",
+    "url": "/app2/Re.png"
+  },
+  {
+    "revision": "cb4d64362e094e69e0f1717d20fb01e9",
+    "url": "/app2/business.png"
+  },
+  {
+    "revision": "86b73c6d5381a15b635232b66ea56ba5",
+    "url": "/app2/cs.jpg"
+  },
+  {
+    "revision": "91499d44269de0e5b3c6",
+    "url": "/app2/css/app.dc378459.css"
+  },
+  {
+    "revision": "6082d5821550c4ddec00",
+    "url": "/app2/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "8a28610f8b9d91d603d09d761d3c1603",
+    "url": "/app2/english.png"
+  },
+  {
+    "revision": "fdbfb4f23ed23d6f365bd63c2c81fa02",
+    "url": "/app2/geography.png"
+  },
+  {
+    "revision": "1d42f4505537de19befa4b30fa2f9509",
+    "url": "/app2/history.png"
+  },
+  {
+    "revision": "49b92d5449faf7e5910a228a86453d88",
+    "url": "/app2/icon.png"
+  },
+  {
+    "revision": "ecbbd4c8f83478761557a1696c90c7f4",
+    "url": "/app2/index.html"
+  },
+  {
+    "revision": "91499d44269de0e5b3c6",
+    "url": "/app2/js/app.f6c744ec.js"
+  },
+  {
+    "revision": "6082d5821550c4ddec00",
+    "url": "/app2/js/chunk-vendors.bef344a5.js"
+  },
+  {
+    "revision": "399fd76c3cf5f376356a9a61463d8bc2",
+    "url": "/app2/lessonApp.webmanifest"
+  },
+  {
+    "revision": "aa487ad2c5df30702370b68d875dd313",
+    "url": "/app2/manifest.json"
+  },
+  {
+    "revision": "afbc51f2a2df62d8c3d4657f3372570a",
+    "url": "/app2/maths.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app2/robots.txt"
+  },
+  {
+    "revision": "6fe604a7da76285e72d380429152374b",
+    "url": "/app2/science.png"
+  }
+]);
